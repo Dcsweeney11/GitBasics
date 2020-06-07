e02c0af860eb8d90aa08486414e0f514079a5adc
dcsweeney11 <abc.123@gmail.com>
2020-06-07 19:09:43 -0400

    commiting change to Test2

--- src/com/leraningGit/syntax/Test2.java
+++ src/com/leraningGit/syntax/Test2.java
@@ -3,7 +3,7 @@ package com.leraningGit.syntax;
 public class Test2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Making a change for remote repo");
 
 	}
 
